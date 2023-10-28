f4fa0a5c8778b7051fd3b5daa654b48fb37435be
Bibek Regmi <1016matrixbibek@gmail.com>
2023-10-28 12:28:46 +0545

    City Service layer added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const {PORT} = require('./config/serverConfig');
-const CityRepository = require('./repository/city-repository')
+// const CityRepository = require('./repository/city-repository')
 
 const setupAndStartServer = async () => {
     const app = express();
@@ -14,8 +14,8 @@ const setupAndStartServer = async () => {
         
         console.log(`Server Started on Port ${PORT}`);
 
-        const repo = new CityRepository();
-        repo.deleteCity(1);
+        // const repo = new CityRepository();
+        // repo.deleteCity(1);
 
     });
 }
