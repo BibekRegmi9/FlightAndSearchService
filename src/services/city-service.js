709eeb361d82432d0d6266bce384b2cff1439746
Bibek Regmi <1016matrixbibek@gmail.com>
2023-10-28 17:14:51 +0545

    Get all cities api is created

--- src/services/city-service.js
+++ src/services/city-service.js
@@ -47,6 +47,16 @@ class CityService {
         }
     }
 
+    async getAllCities(){
+        try {
+            const cities = await this.cityRepository.getAllCities();
+            return cities;
+        } catch (error) {
+            console.log("Something went wrong in Service layer");
+            throw {error};
+        }
+    }
+
 }
 
 
